5c8dd2f8afdca03d81bfe8629777060797db3ad3
philfanzhou <philfanzhou@hotmail.com>
2016-01-24 15:26:08 +0800

    更新动态库和icon

--- 3_Application/Telephone.Application.Information/DataReader.cs
+++ 3_Application/Telephone.Application.Information/DataReader.cs
@@ -1,50 +1,56 @@
 ﻿using Ore.Infrastructure.MarketData;
-using Pitman.Presentation.RESTfulClient;
+using Pitman.RESTful.Client;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Telephone.Application.Information
 {
     public class DataReader
     {
-        private string serverAddress = "http://quantum1234.cloudapp.net:6688";
+        private string serverAddress = "http://quantum1234.cloudapp.net:6688/api";
 
         //获取所有服务名称
         public IEnumerable<string> GetCollectionServices()
         {
-            var client = ClientFactory.CreateCollectionStatusClient(serverAddress);
-            return client.GetAllServiceName();
+            using (var client = new ClientApi(serverAddress))
+            {
+                return client.GetAllServiceName();
+            }
         }
 
         //获取指定服务状态
         public string GetServiceStatus(string serviceName)
         {
-            var client = ClientFactory.CreateCollectionStatusClient(serverAddress);
-            return client.GetStatus(serviceName);
+            using (var client = new ClientApi(serverAddress))
+            {
+                return client.GetServiceStatus(serviceName);
+            }
         }
 
         //获取股票实时数据
         public IEnumerable<IStockRealTime> GetStockRealTimeData(IEnumerable<string> stockCodes)
         {
-            var client = ClientFactory.CreateRealTimeClient(serverAddress);
-            return client.GetLatest(stockCodes).Cast<IStockRealTime>();
+            //var client = ClientFactory.CreateRealTimeClient(serverAddress);
+            //return client.GetLatest(stockCodes).Cast<IStockRealTime>();
+
+            /*调用Sina API进行实现，引用已添加*/
+            throw new NotImplementedException();
         }
 
         //获取分时数据
         public IEnumerable<IStockIntraday> GetIntradayData(string stockCode, DateTime startDate, DateTime endDate)
         {
-            var client = ClientFactory.CreateIntradayClient(serverAddress);
-            return client.GetData(stockCode, startDate, endDate).Cast<IStockIntraday>();
+            /*暂未实现*/
+            throw new NotImplementedException();
         }
 
         //获取K线1分钟数据
         public IEnumerable<IStockKLine> GetKLineBy1Minute(string stockCode, DateTime startDate, DateTime endDate)
         {
-            var client = ClientFactory.CreateKLineClient(serverAddress);
-            return client.GetBy1Minute(stockCode, startDate, endDate).Cast<IStockKLine>();
+            using (var client = new ClientApi(serverAddress))
+            {
+                return client.GetStockKLine(KLineType.Min1, stockCode, startDate, endDate);
+            }
         }
     }
 }
